c6c63e4e9231d40b204aaec1354f1fc00853d9ee
joseph.cota <joesph.cota@kearsarge.org>
2022-10-25 08:02:16 -0400

    set up

---
+++ Getters.py
@@ -0,0 +1,104 @@
+def getMenuOption(debug = False):
+    if debug: print("getMenuOption Function")
+    
+    goodInput = False
+    
+    while not goodInput:
+        option = input("Please select an option: ")
+        option = option.lower()
+        
+        if (option == "q" or 
+            option == "quit" or 
+            option == "x" or
+            option == "exit"):
+                option = "q"
+                goodInput = True
+        elif (option == "1" or 
+            option == "one" or 
+            option == "story 1" or
+            option == "story1"):
+                option = "1"
+                goodInput = True        
+                
+            
+        else:
+            print("Please make a valid choice")
+        
+    return option
+
+def getWord(prompt, debug = False):
+    if debug: print("getWord Function")
+    
+    goodInput = False
+    
+    while not goodInput:
+        word = input(prompt)
+        goodInput = True
+        if isSwear(word, debug):
+            goodInput = False
+            print ("UNACCEPTABLE")
+        
+    return word
+    
+def getSport(prompt, debug = False):
+    if debug: print("getSport Function")
+    
+    goodInput = False
+    
+    sports = ["soccer", 
+              "football",
+              "hockey",
+              "wrestling",
+              "lacrosse",
+              "baseball",
+              "softball",
+              "track and field",
+              "volleyball",
+              "skiing",
+              "snowboarding",
+              "surfing",
+              "throwing stuff",
+              "halo",
+              "sleding",
+              "hiking",
+              "catch",
+              "video game/s",
+              "throwing",
+              "chess",
+              "kickball",
+              "checkers",
+              "bass fishing",
+              ]
+    
+    while not goodInput:
+        word = input(prompt)
+        goodInput = True
+        if isSwear(word, debug):
+            goodInput = False
+            print ("UNACCEPTABLE")
+        elif word.lower() not in sports:
+            goodInput = False
+            print ("INVALID")
+        
+    return word
+    
+def isSwear(word, debug = False):
+    if debug: print("isSwear Function")
+    if word.lower() in swearList:
+        return True
+    else:
+        return False
+
+swearList = ["poop",
+             "pee",
+             "shit",
+             "fuck",
+             "ass",
+             "chicken",
+             "cheese",
+             "motherfucker",
+             "fucker",
+             "dick",
+             "asshole",
+             "dipstick",
+]
---
+++ Story1.py
@@ -0,0 +1,65 @@
+from Getters import *
+
+def Story1(debug = False):
+    if debug: print("Story1 Function")
+    
+    print("\n")
+    friendName1 = getWord("Enter a name: ", debug)
+    sport1 = getSport("Enter a sport: ", debug)
+    pluralnoun1 = getWord("Enter a noun: ", debug)
+    place1 = getWord("Enter a place: ", debug)
+    verb1 = getWord("Enter a verb: ", debug)
+    thing1 =getWord("Enter a thing: ", debug)
+    # ~ adverb1 = getWord("Enter a adverb: ", debug)
+    # ~ pronoun1 = getWord("Enter a pronoun: ", debug)
+    # ~ place2 = getWord("Enter a place: ", debug)
+    # ~ adjective1 = getWord("Enter a adjective: ", debug)
+    # ~ noun1 = getWord("Enter a noun: ", debug)
+    # ~ verb2 = getWord("Enter a verb: ", debug)
+    # ~ date1 = getWord("Enter a date: ", debug)
+    # ~ number1 = getWord("Enter a number: ", debug)
+    # ~ friendName2 = getWord("Enter a name: ", debug)
+    # ~ noun2 = getWord("Enter a noun: ", debug)
+    # ~ noun3 = getWord("Enter a noun: ", debug)
+    # ~ verb3 = getWord("Enter a verb: ", debug)
+    # ~ adverb2 = getWord("Enter a adverb: ", debug)
+    # ~ noun4 = getWord("Enter a noun: ", debug)
+    # ~ color1 = getWord("Enter a color: ", debug)
+    # ~ color2 = getWord("Enter a color: ", debug)
+    # ~ pluralthing2 = getWord("Enter a thing: ", debug)
+    # ~ adverb3 = getWord("Enter a adverb: ", debug)
+    # ~ verb4 = getWord("Enter a verb: ", debug)
+    # ~ building1 = getWord("Enter a building: ", debug)
+    
+    
+    
+    out = "\n"
+    out += "One day me and my friend " + friendName1
+    out += " were out playing " + sport1
+    out += " \n and it started to rain " + pluralnoun1
+    out += " at my " + place1
+    out += " \n and we " + verb1
+    out += " screaming " + thing1
+    out += " until the rain stopped. The End. "
+    # ~ out += "Then we " + adverb1
+    # ~ out += " \n went outside and " + pronoun1
+    # ~ out += " We had to move to " + place2
+    # ~ out += " \n to get away from the " + adjective1 + noun1
+    # ~ out += " that my uncle " + verb2 
+    # ~ out += " away on " + date1
+    # ~ out += " \n " + number1
+    # ~ out += " days later " + friendName2
+    # ~ out += " showed up at my " + noun2
+    # ~ out += " holding a " + noun3
+    # ~ out += " \n screaming " + verb3
+    # ~ out += " as multiple police cars " + adverb2
+    # ~ out += " drove down the " + noun4
+    # ~ out += " and all you see is " + color1
+    # ~ out += " and " + color2
+    # ~ out += " \n Out of nowhere " + pluralthing2
+    # ~ out += " are falling from the sky so you " + adverb3 + verb4
+    # ~ out += " to the nearest " + building1
+    
+    
+    
+    return out
