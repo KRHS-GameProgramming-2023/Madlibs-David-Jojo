cfdded68a896111bdc99632ee8c381fb0fd21d79
Joseph Cota <joseph.cota@kearsarge.org>
2022-10-28 09:10:31 -0400

    Added more to vehicle code and edited the story

--- Story2.py
+++ Story2.py
@@ -7,17 +7,17 @@ def Story2(debug = False):
     friendName1 = getWord("Enter a name: ", debug)
     vehicle1 = getVehicle("Enter a vehicle: ", debug)
     object1 = getWord("Enter a object: ", debug)
-    vehicle2 = getWord("Enter a vehicle: ", debug)
-    time1 = getWord("Enter a time: ", debug)
+    thing1 = getWord("Enter a thing: ", debug)
+    time1 = getWord("Enter a minute: ", debug)
     building1 = getWord("Enter a building: ", debug)
     
     
     out = "\n"
     out += " One day me and my friend " + friendName1
     out += " were out driving his " + vehicle1
-    out += " He wasn't paying attention and crashed into a " + object1
-    out += " then we called the E.M.T and when they got there they loaded us into a " + vehicle2
-    out += " It took " + time1
+    out += " \n He wasn't paying attention and crashed into a " + object1
+    out += " then we called the E.M.T's and when they got there \n they loaded us into a " + thing1
+    out += " \n It took " + time1
     out += " for the E.M.T's to arrive at " + building1
     
     return out
