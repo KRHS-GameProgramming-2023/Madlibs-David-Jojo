57bef5b058283eac0f6c23b9bb3d03f8ab4c70a8
Joseph Cota <joseph.cota@kearsarge.org>
2022-11-01 08:38:09 -0400

    Story 3

---
+++ Story3.py
@@ -0,0 +1,13 @@
+from Getters import *
+
+def Story3(debug = False):
+    if debug: print("Story3 Function")
+
+    print("\n")
+    friendName1 = getWord("Enter a name: ", debug)
+    
+    
+    out = "\n"
+    
+    
+    return out
