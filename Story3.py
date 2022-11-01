0d975a164edd288ea65bab5d786dc86f39d6ee57
Joseph Cota <joseph.cota@kearsarge.org>
2022-11-01 09:05:43 -0400

    Working on Story 3

--- Story3.py
+++ Story3.py
@@ -5,9 +5,13 @@ def Story3(debug = False):
 
     print("\n")
     friendName1 = getWord("Enter a name: ", debug)
+    clothingstore1 = getWord("Enter a clothesstore: ", debug)
+    thing1 = getWord("Enter a thing: ", debug)
     
     
     out = "\n"
-    
+    out += " Once upon a time my friend " + friendName1
+    out += " and I went to " + clothingstore1
+    out += " \n To our suprise there was " + thing1
     
     return out
