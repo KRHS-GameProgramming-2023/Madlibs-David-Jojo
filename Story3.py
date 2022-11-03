71efcabe0e9f87f57fda146cf8a9282b67a3e389
David Patten <david.patten@keararge.org>
2022-11-03 08:36:49 -0400

    Added to story three

--- Story3.py
+++ Story3.py
@@ -5,13 +5,16 @@ def Story3(debug = False):
 
     print("\n")
     friendName1 = getWord("Enter a name: ", debug)
-    clothingstore1 = getWord("Enter a clothesstore: ", debug)
+    clothingstore1 = getWord("Enter a clothing store: ", debug)
     thing1 = getWord("Enter a thing: ", debug)
+    place1 = getWord("Enter a place: ",debug)
     
     
     out = "\n"
     out += " Once upon a time my friend " + friendName1
     out += " and I went to " + clothingstore1
-    out += " \n To our suprise there was " + thing1
+    out += " \n and to our suprise there was " + thing1
+    out += " We bought it and took it to the " + place1
+    out += " and we used our new " +thing1
     
     return out
