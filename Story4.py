b892d9a9c0ce4c106ad0fd6d103c7ec89286a32d
David Patten <david.patten@keararge.org>
2022-11-04 09:06:55 -0400

    Added easter egg

---
+++ Story4.py
@@ -0,0 +1,15 @@
+from Getters import *
+
+def Story4(debug = False):
+    if debug: print("Story4 Function")
+
+    print("\n")
+    number1 = getNumber("Enter a number between 1 and 100: ", debug)
+
+    
+    
+    out = "\n"
+    out += " Congraguloations you just won $3,000,000  " 
+
+    
+    return out
